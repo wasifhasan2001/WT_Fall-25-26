3d804102515322a48f60b3cb29c9d072a66b06f0
Wasif Hasan <wasifhasan2001@gmail.com>
2025-11-02 12:30:59 +0600

    first change

--- Mid/Lab Task 1/lab1.php
+++ Mid/Lab Task 1/lab1.php
@@ -5,7 +5,46 @@
     <title>Document</title>
 </head>
 <body>
+    <center> 
+        <h1> Clinic Patient Registration </h1>
+
+<table>
+    <td> 
+        
+        Full Name: <br>   
+        <input type="text"> <br>
+        Age: <br>
+        <input type="text"> <br>
+        Phone Number: <br>
+        <input type="text"> <br>
+        Email Address: <br>
+        <input type="text"> <br>
+        Insurance Provider: <br>
+        <select>
+            <option> select provider </option>
+            <option>a</option>
+            <option>a</option>
+
+        </select> <br>
+        Insurance Policy Number: <br>
+         <input type="text"> <br>
+
+
+    </td>
+</table>
     
+        <h2> Additional Information </h2>
+ <table>   
+    <td>
+        Username: <br>
+        <input type="text"> <br>
+        Password: <br>
+        <input type="text"> <br>
+        Confirm Password: <br>
+        <input type="text"> <br>
+    </td>
     
+</table>
+    </center>
 </body>
 </html>
