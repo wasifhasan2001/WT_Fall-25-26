195b79ddd5b670f501af32599f984443aad8d8c1
Wasif Hasan <wasifhasan2001@gmail.com>
2025-11-16 11:45:42 +0600

    lab2

--- Mid/Lab Task 2/lab2.php
+++ Mid/Lab Task 2/lab2.php
@@ -7,7 +7,33 @@
 </head>
 <body>
     <center>
-    <h1>Participant Registration</h1>
+    <h2>Participant Registration</h2>
+    <table>
+        <td>
+
+        Full Name: <br>   
+        <input type="text"> <br>
+        Email: <br>
+        <input type="text"> <br>
+        Phone Number: <br>
+        <input type="number"> <br>
+        Password: <br>
+        <input type="text"> <br>
+        Confirm Password: <br>
+        <input type="text"> <br>
+        <button type="submit" style="background-color: blue;">Register</button>
+        </td>
+    </table>
+
+    <h2>Activity Selection</h2>
+    <table>
+        <td>
+        Activity Name: <br>   
+        <input type="text"> <br>
+         <button type="submit" style="background-color: blue;">Add Activity</button>
+        </td>
+    </table>
     </center>
+    
 </body>
 </html>
