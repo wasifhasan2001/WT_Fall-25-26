5b15ea5142d6b7620afe66ec5e8b366a30ba10bd
Wasif Hasan <wasifhasan2001@gmail.com>
2026-01-19 20:48:00 +0600

    stats con

--- PlayVerse/Management/User/MVC/php/stats_controller.php
+++ PlayVerse/Management/User/MVC/php/stats_controller.php
@@ -5,12 +5,12 @@ header('Content-Type: application/json');
 
 $action = $_POST['action'] ?? '';
 
-/* Handle view telemetry */
+
 if ($action === 'track_view') {
     $gameId = (int)($_POST['game_id'] ?? 0);
     
     if ($gameId > 0) {
-        /* Update popularity metrics */
+   
         incrementPopularity($gameId);
         echo json_encode(["status" => "success"]);
     } else {
