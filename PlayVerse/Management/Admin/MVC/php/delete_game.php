d33799391ee53dac9a0a8f2bad830efe9ce0456c
Wasif Hasan <wasifhasan2001@gmail.com>
2026-01-19 17:45:25 +0600

    dlt gm

--- PlayVerse/Management/Admin/MVC/php/delete_game.php
+++ PlayVerse/Management/Admin/MVC/php/delete_game.php
@@ -4,19 +4,19 @@ requireRole('admin');
 
 require_once('../db/gameModel.php');
 
-/* Sanitize input ID */
+
 $id = (int)($_GET['id'] ?? 0);
 
 if($id > 0){
     
-    /* Execute record deletion */
+   
     if(deleteGame($id)){
         header("Location: ../html/admin_dashboard.php?msg=" . urlencode("Game deleted"));
     } else {
         header("Location: ../html/admin_dashboard.php?msg=" . urlencode("Delete failed"));
     }
 } else {
-    /* Invalid ID redirect */
+  
     header("Location: ../html/admin_dashboard.php");
 }
 exit();
