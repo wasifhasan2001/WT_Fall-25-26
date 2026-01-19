f600a974eabdbeb3f0c9e76fb7902e9e9850ad6d
Wasif Hasan <wasifhasan2001@gmail.com>
2026-01-19 17:58:28 +0600

    logout

--- PlayVerse/Management/Auth/MVC/php/logout.php
+++ PlayVerse/Management/Auth/MVC/php/logout.php
@@ -3,10 +3,10 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-/* Clear session data */
+
 session_unset();
 session_destroy();
 
-/* Redirect to login */
+
 header("Location: ../html/login.php");
 exit();
