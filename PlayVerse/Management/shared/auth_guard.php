6cbc77df0cebf7caea6024563291181c365cf04d
Wasif Hasan <wasifhasan2001@gmail.com>
2026-01-19 18:02:30 +0600

    auth g

--- PlayVerse/Management/shared/auth_guard.php
+++ PlayVerse/Management/shared/auth_guard.php
@@ -3,16 +3,16 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-/* Enforce role-based access */
+
 function protect_page($allowed_role) {
     if (!isset($_SESSION['user_id'])) {
-        /* Redirect unauthorized guest */
+
         header("Location: ../../../Auth/MVC/html/login.php");
         exit();
     }
 
     if ($_SESSION['role'] !== $allowed_role) {
-        /* Redirect incorrect role */
+
         if ($_SESSION['role'] === 'admin') {
             header("Location: ../../../Admin/MVC/html/admin_dashboard.php");
         } else {
@@ -22,7 +22,7 @@ function protect_page($allowed_role) {
     }
 }
 
-/* Alias for protection */
+
 function requireRole($role) {
     protect_page($role);
 }
